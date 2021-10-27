726f8e235b3ca2ef19b3175e0689f6a7af12fbf8
Andrei <andrei.fajardo@gmail.com>
2021-10-27 00:23:41 -0400

    add pylintrc for project, and lint utils.py 10/10 score

--- fedjax_tutorial/src/utils.py
+++ fedjax_tutorial/src/utils.py
@@ -1,26 +1,33 @@
+"""utility functions commonly shared by example scripts"""
+
 import json
 import logging
+import sys
 from functools import wraps
 from time import time
 
-# timing wrapper modified code from 
+# timing wrapper modified code from
 # https://stackoverflow.com/questions/1622943/timeit-versus-timing-decorator
-def timing(f):
-    @wraps(f)
+def timing(func):
+    """time wrapper for calculating execution time"""
+    @wraps(func)
     def wrap(*args, **kw):
-        ts = time()
-        result = f(*args, **kw)
-        te = time()
-        print('func:{} took: {:2.4f} sec'.format(f.__name__, te-ts))
-        logging.info('func:{} took: {:2.4f} sec'.format(f.__name__, te-ts))
+        time_start = time()
+        result = func(*args, **kw)
+        time_end = time()
+        print('func:{} took: {:2.4f} sec'\
+            .format(func.__name__, time_end-time_start))
+        logging.info('func:{} took: {:2.4f} sec'\
+            .format(func.__name__, time_end-time_start))
         return result
     return wrap
 
 def load_config(file_name):
+    """utility function to load json config"""
     with open('configs/{}.json'.format(file_name)) as f:
         try:
             model_params = json.load(f)
-        except:
+        except: # pylint: disable=bare-except
             print("Cannot load content of {}.json".format(file_name))
             sys.exit(1)
     return model_params
